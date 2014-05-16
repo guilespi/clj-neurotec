1b904ada1700287ef43d24ddac9eb537ad2ed876
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-05-16 17:23:05 -0300

    create templates from images on disk
    this stills needs review since more than one image may belong to the same subject, need to consider that and create only one template with all fingers for each subject

--- test/clj_neurotec/samples/enroll_from_disk.clj
+++ test/clj_neurotec/samples/enroll_from_disk.clj
@@ -2,20 +2,24 @@
   (:gen-class)
   (:require [clj-neurotec.client :as c]
             [clj-neurotec.subject :as s]
+            [clj-neurotec.template :as t]
             [clj-neurotec.util :as u]
             [clojure.java.io :as io]))
 
 
+(defmacro traverse-and-apply
+  [dir fun]
+  `(let [files# (rest (file-seq (io/file ~dir)))]
+     (doseq [f# files#]
+       (let [finger# (s/finger-from-file (.getPath f#))
+             subject# (s/make-subject {:id (.getName f#)
+                                       :fingers [finger#]})]
+         (~fun subject#)))))
+
 (defn enroll-from-dir
   [client dir]
   (println "Enrolling from dir" dir)
-  (let [files (take 1 (file-seq (io/file dir)))]
-    (doseq [f files]
-      (let [finger (s/finger-from-file (.getPath f))
-            subject (s/make-subject {:id (.getName f)
-                                     :fingers [finger]})]
-        (println "Enrolling subject" subject)
-        (c/enroll client subject)))))
+  (traverse-and-apply dir (partial c/enroll client)))
 
 (defn identify-from-file
   [client filename]
@@ -26,6 +30,19 @@
     (println "Identifying subject" subject)
     (c/identify client subject)))
 
+(defn save-template
+  [dir subject template]
+  (let [path (str dir "/" (.getId subject) ".dat")
+        _ (println "Saving template" path)]
+    (t/save template path)))
+
+(defn templates-from-dir
+  [client dir]
+  (letfn [(create-template [subject]
+            (when-let [tmp (c/create-template client subject)]
+              (save-template dir subject tmp)))]
+    (traverse-and-apply dir create-template)))
+
 (defn -main
   []
   (println "Running enroll from disk")
@@ -33,3 +50,10 @@
   (let [client (c/make-client {})]
     (enroll-from-dir client "/home/guilespi/Dbs/DB4_A")
     (identify-from-file client "/home/guilespi/Dbs/DB4_A/9_10.bmp")))
+
+
+(comment
+  (let [client (c/make-client {})]
+    (templates-from-dir client "/home/guilespi/Dbs/DB4_A"))
+
+  )
