56b145f01caaed3f52174b197b90184cf5ef4083
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-05-16 12:36:38 -0300

    make enroll-from-disk runnable

--- test/clj_neurotec/samples/enroll_from_disk.clj
+++ test/clj_neurotec/samples/enroll_from_disk.clj
@@ -1,26 +1,35 @@
 (ns clj-neurotec.samples.enroll-from-disk
-  (:require [clj-neurotec.client :as client]
-            [clj-neurotec.subject :as subject]
+  (:gen-class)
+  (:require [clj-neurotec.client :as c]
+            [clj-neurotec.subject :as s]
+            [clj-neurotec.util :as u]
             [clojure.java.io :as io]))
 
 
 (defn enroll-from-dir
-  [cli dir]
+  [client dir]
+  (println "Enrolling from dir" dir)
   (let [files (take 1 (file-seq (io/file dir)))]
     (doseq [f files]
-      (let [finger (subject/finger-from-file (.getPath f))
-            sbj (subject/make-subject {:id (.getName f)
-                                       :fingers [finger]})]
-        (client/enroll cli sbj)))))
+      (let [finger (s/finger-from-file (.getPath f))
+            subject (s/make-subject {:id (.getName f)
+                                     :fingers [finger]})]
+        (println "Enrolling subject" subject)
+        (c/enroll client subject)))))
 
 (defn identify-from-file
-  [cli filename]
-  (let [finger (subject/finger-from-file filename)
-        sbj (subject/make-subject {:id (str (java.util.UUID/randomUUID))
+  [client filename]
+  (println "Identifying from file" filename)
+  (let [finger (s/finger-from-file filename)
+        subject (s/make-subject {:id (str (java.util.UUID/randomUUID))
                                    :fingers [finger]})]
-    (client/identify cli sbj)))
+    (println "Identifying subject" subject)
+    (c/identify client subject)))
 
-(comment
-  (let [bio-cli (client/make-client {})]
-    (enroll-from-dir bio-cli "/home/guilespi/Dbs/DB4_A")
-    (identify-from-file bio-cli "/home/guilespi/Dbs/DB4_A/9_10.bmp")))
+(defn -main
+  []
+  (println "Running enroll from disk")
+  (u/init-library-path!)
+  (let [client (c/make-client {})]
+    (enroll-from-dir client "/home/guilespi/Dbs/DB4_A")
+    (identify-from-file client "/home/guilespi/Dbs/DB4_A/9_10.bmp")))
