7c9c1c33c5573d75628d31e75d2feb28108e906c
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-05-19 15:46:23 -0300

    project version fix

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-neurotec "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-neurotec "0.2.0"
+  :description "Neurotec Java Bindings"
+  :url "https://github.com/guilespi/clj-neurotec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
