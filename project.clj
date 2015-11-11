0ee0805ac6f22daea6fedab9ac38c3ef5297fc72
Guillermo Winkler <guillermo.winkler@gmail.com>
2015-11-11 13:18:33 -0300

    depend on clojure 1.7 and neurotech 6.0

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject clj-neurotec "0.2.0"
+(defproject clj-neurotec "0.2.1"
   :description "Neurotec Java Bindings"
   :url "https://github.com/guilespi/clj-neurotec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.neurotec/neurotec-core "5.0.0.0"]
-                 [com.neurotec/neurotec-cluster "5.0.0.0"]
-                 [com.neurotec/neurotec-licensing "5.0.0.0"]
-                 [com.neurotec/neurotec-media "5.0.0.0"]
-                 [com.neurotec/neurotec-media-processing "5.0.0.0"]
-                 [com.neurotec/neurotec-devices "5.0.0.0"]
-                 [com.neurotec/neurotec-biometrics "5.0.0.0"]
-                 [com.neurotec/neurotec-biometrics-client "5.0.0.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.neurotec/neurotec-core "6.0.0.0"]
+                 [com.neurotec/neurotec-cluster "6.0.0.0"]
+                 [com.neurotec/neurotec-licensing "6.0.0.0"]
+                 [com.neurotec/neurotec-media "6.0.0.0"]
+                 [com.neurotec/neurotec-media-processing "6.0.0.0"]
+                 [com.neurotec/neurotec-devices "6.0.0.0"]
+                 [com.neurotec/neurotec-biometrics "6.0.0.0"]
+                 [com.neurotec/neurotec-biometrics-client "6.0.0.0"]
                  [net.java.dev.jna/jna "3.5.1"]])
