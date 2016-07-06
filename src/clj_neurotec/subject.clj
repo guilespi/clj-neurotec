d0ab081810e0694e85867c9f694d090fd1849c8d
Fabian Peña <fapenia@gmail.com>
2016-07-06 16:45:00 -0300

    Fix finger-positions enums

--- src/clj_neurotec/subject.clj
+++ src/clj_neurotec/subject.clj
@@ -10,10 +10,10 @@
               :unspecified NGender/UNSPECIFIED})
 
 (def finger-positions {:right-index-finger NFPosition/RIGHT_INDEX_FINGER
-                       :right-middle-finger NFPosition/LEFT_MIDDLE_FINGER
-                       :right-ring-finger NFPosition/LEFT_RING_FINGER
-                       :right-little-finger NFPosition/LEFT_LITTLE_FINGER
-                       :right-thumb-finger NFPosition/LEFT_THUMB
+                       :right-middle-finger NFPosition/RIGHT_MIDDLE_FINGER
+                       :right-ring-finger NFPosition/RIGHT_RING_FINGER
+                       :right-little-finger NFPosition/RIGHT_LITTLE_FINGER
+                       :right-thumb-finger NFPosition/RIGHT_THUMB
                        :left-index-finger NFPosition/LEFT_INDEX_FINGER
                        :left-middle-finger NFPosition/LEFT_MIDDLE_FINGER
                        :left-ring-finger NFPosition/LEFT_RING_FINGER
